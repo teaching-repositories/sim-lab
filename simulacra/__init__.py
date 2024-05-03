62cf0a67ec59e9c5fb931a0674a2068970fbc7c3
Michael Borck <michael.borck@curtin.edu.au>
2024-05-03 16:25:06 +0800

    import classes directly

--- simulacra/__init__.py
+++ simulacra/__init__.py
@@ -1,3 +1,9 @@
 from .product_popularity_simulation import ProductPopularitySimulation
 from .resource_fluctuations_simulation import ResourceFluctuationsSimulation
 from .stock_market_simulation import StockMarketSimulation
+
+__all__ = [
+    "ProductPopularitySimulation",
+    "ResourceFluctuationsSimulation",
+    "StockMarketSimulation"
+]
