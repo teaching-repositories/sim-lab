a7ea2bb82310901119ac719cb4d42bd510ad0aba
Michael Borck <michael.borck@curtin.edu.au>
2024-05-03 09:25:00 +0800

    fixed typo in __init__.py

--- simulacra/__init__.py
+++ simulacra/__init__.py
@@ -1,3 +1,3 @@
 from .product_popularity_simulation import ProductPopularitySimulation
-from .resource_fluctuations_+simulation import ResourceFluctuationsSimulation
+from .resource_fluctuations_simulation import ResourceFluctuationsSimulation
 from .stock_market_simulation import StockMarketSimulation
